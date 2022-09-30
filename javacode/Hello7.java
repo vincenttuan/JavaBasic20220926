d6fc028270a30881e04732003190a910a2d67559
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-09-30 21:44:41 +0800

    Add files via upload

---
+++ javacode/Hello7.java
@@ -0,0 +1,15 @@
+public class Hello7 {
+
+	public static void main(String[] args) {
+		// 浮點數
+		double x = 3.14;
+		System.out.println(x);
+
+		float y = 3.14f;
+		System.out.println(y);
+
+		float z = (float)3.14;
+		System.out.println(z);
+
+	}
+}
