40faae0c49668b42d4e5297449b542f00aa69f04
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-10-03 19:27:21 +0800

    Add files via upload

---
+++ javacode/exp/Demo1.java
@@ -0,0 +1,18 @@
+package exp;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		System.out.println("標準數學運算子~");
+		int x = 5;
+		int y = 2;
+		System.out.println(x + y); // 7
+		System.out.println(x - y); // 3
+		System.out.println(x * y); // 10
+		System.out.println(x / y); // 2
+		System.out.println(x % y); // 1
+		System.out.println(1 / 2); // 0
+		System.out.println(1.0 / 2); // 0.5
+		System.out.println(1 / 2.0); // 0.5
+		System.out.println(1.0 / 2.0); // 0.5
+	}
+}
