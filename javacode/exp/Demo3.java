3f17c7fdbcef737b6236f31194d9f9c287f554b6
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-10-03 20:04:58 +0800

    Add files via upload

---
+++ javacode/exp/Demo3.java
@@ -0,0 +1,15 @@
+package exp;
+
+public class Demo3 {
+	public static void main(String[] args) {
+		System.out.println("數學遞增運算子 ++");
+		int x = 10;
+		int y = ++x; // 前序遞增, x 先加 1 之後再指派給 y
+		System.out.printf("x=%d y=%d\n", x, y);
+
+		int a = 10;
+		int b = a++; // 後序遞增, a 先將內容給 b, 之後 a 再加 1
+		System.out.printf("a=%d b=%d\n", a, b);
+
+	}
+}
