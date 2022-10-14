92d44d934dc572a8bb0f97cc8254e79a52bfb3ea
VincentTuan <vincentjava@yahoo.com.tw>
2022-10-14 18:57:23 +0800

    Add files via upload

--- javacode/loop/WhileDemo1.java
+++ javacode/loop/WhileDemo1.java
@@ -6,8 +6,14 @@ public class WhileDemo1 {
 	public static void main(String[] args) {
 		
 		Random random = new Random(); // 建立一個隨機數物件
-		int num = random.nextInt(100); // 取一個 0~99 的隨機數
-		System.out.println(num);
+
+		while(true) {
+			int num = random.nextInt(100); // 取一個 0~99 的隨機數
+			System.out.println(num);
+			if(num == 27) {
+				break; // 跳離 while 迴圈
+			}
+		}
 
 	}
 }
