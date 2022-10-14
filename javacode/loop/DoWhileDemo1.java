09c6c3f93031c436b04983339512db5982d845f6
VincentTuan <vincentjava@yahoo.com.tw>
2022-10-14 21:12:25 +0800

    Add files via upload

--- javacode/loop/DoWhileDemo1.java
+++ javacode/loop/DoWhileDemo1.java
@@ -11,11 +11,11 @@ public class DoWhileDemo1 {
 		do {
 			// user 猜
 			Scanner scanner = new Scanner(System.in);
-			System.out.printf("請在 %d~%d 之間猜一數字: ", min, max);
+			System.out.printf("User 請在 %d~%d 之間猜一數字: ", min, max);
 			int userGuess = scanner.nextInt();
 			// 檢查 userGuess 是否合理 ?
 			if(userGuess >= max || userGuess <= min) {
-				System.out.println("數字範圍錯誤, 請重新猜~");
+				System.out.println("User 輸入數字範圍錯誤, 請重新猜~");
 				continue;
 			}
 			// 判斷 user 是否有猜中 ?
@@ -24,7 +24,18 @@ public class DoWhileDemo1 {
 			} else if(userGuess > ans) {
 				max = userGuess;
 			} else {
-				System.out.println("恭喜 ! 答對了 ~");
+				System.out.println("恭喜 User 答對了 ~");
+				break;
+			}
+			// pc 猜
+			int pcGuess = random.nextInt(max - min - 1) + min + 1;
+			System.out.printf("PC 請在 %d~%d 之間猜一數字: %d\n", min, max, pcGuess);
+			if(pcGuess < ans) {
+				min = pcGuess;
+			} else if(pcGuess > ans) {
+				max = pcGuess;
+			} else {
+				System.out.println("恭喜 PC 答對了 ~");
 				break;
 			}
 
