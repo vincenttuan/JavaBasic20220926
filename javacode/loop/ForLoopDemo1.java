b1bb636b5676b92a5c5a66407d5dd5927a99e27e
VincentTuan <vincentjava@yahoo.com.tw>
2022-10-14 21:31:04 +0800

    Add files via upload

---
+++ javacode/loop/ForLoopDemo1.java
@@ -0,0 +1,12 @@
+package loop;
+
+public class ForLoopDemo1 {
+
+	public static void main(String[] args) {
+		// 利用 for-loop 印出 1~10
+		for( int x = 1 ; x <= 10 ; x++ ) {
+			System.out.println(x);
+		}
+	}
+
+}
