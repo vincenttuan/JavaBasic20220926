3d4b6ed0f9214a522fafebf10fbc9142ebe9754b
VincentTuan <vincentjava@yahoo.com.tw>
2022-10-14 18:52:08 +0800

    Add files via upload

---
+++ javacode/loop/WhileDemo1.java
@@ -0,0 +1,13 @@
+package loop;
+
+import java.util.Random; // 匯入隨機數資源
+
+public class WhileDemo1 {
+	public static void main(String[] args) {
+		
+		Random random = new Random(); // 建立一個隨機數物件
+		int num = random.nextInt(100); // 取一個 0~99 的隨機數
+		System.out.println(num);
+
+	}
+}
