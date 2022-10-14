d85fef179fe64bb7feddf6817074affe911ce1b5
VincentTuan <vincentjava@yahoo.com.tw>
2022-10-14 21:46:05 +0800

    Add files via upload

---
+++ javacode/loop/Ninetable.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class Ninetable {
+	public static void main(String[] args) {
+		
+		for(int x=1 ; x <= 9 ; x++) {
+
+			for(int y=1 ; y<=9 ; y++) {
+				System.out.printf("%2d * %2d = %2d  ", x, y, (x*y));
+			}
+			System.out.println();
+
+		}
+
+	}
+}
